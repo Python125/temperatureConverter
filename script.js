7976d2c8504ccf9ad502cc506a420a63c5ad5c8d
Python125 <91922582+Python125@users.noreply.github.com>
2022-06-07 13:14:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function convertToK(fahrenheit) {
   return kelvin
 }
 console.log(convertToK(0))
+
+function convertToF(celsius) {
+  let fahrenheit = (celsius*1.8)+32;
+  return fahrenheit;
+};
+
+console.log(convertToF(0));
+
+function convertToK(celsius) {
+  let kelvin = celsius + 273.15;
+  return kelvin;
+};
+
+console.log(convertToK(-273.15));
+
